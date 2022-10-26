7bd0460713badee2808fb54f813028f6dd9f5402
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2022-10-26 15:53:44 -0500

    Removed unnecessary files and content

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import React from "react";
 import WeatherSearch from "./WeatherSearch";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <h2>Coded by: Gabriella Corales (EllaCodes)</h2>
+
       <WeatherSearch />
     </div>
   );
 }
-
-export default App;
